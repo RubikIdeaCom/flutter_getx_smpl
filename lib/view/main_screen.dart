d4fe55552485280fa9868c04b938d748912068b2
Ali Seyedabadi <rubikideacom@gmail.com>
2022-12-28 22:41:10 +0330

    Model added, controller? Not so fast!

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_smpl/model/product_model.dart';
 import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
 
-  var counter = 0.obs;
+  Rx<ProductModel> prodcutModel =
+      ProductModel(name: "پفک نمکی", price: "200000", off: "10%").obs;
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +16,19 @@ class MainScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Obx((() {
-              return Text('Counter: $counter',
-                  style: const TextStyle(fontSize: 30));
+              return Column(
+                children: [
+                  Text('Product name: ${prodcutModel.value.name}',
+                      style: const TextStyle(fontSize: 20)),
+                  Text('Product price: ${prodcutModel.value.price}',
+                      style: const TextStyle(fontSize: 20)),
+                  Text('Product off: ${prodcutModel.value.off}',
+                      style: const TextStyle(fontSize: 20)),
+                ],
+              );
             })),
             ElevatedButton(
-              onPressed: () {
-                counter = counter + 1;
-              },
+              onPressed: () {},
               child: null,
             )
           ],
